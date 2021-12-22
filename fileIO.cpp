ebb32efe72e08beb50e97f12d0a1f042183dddb7
Memorywzd <1746607977@qq.com>
2021-12-22 12:26:42 +0800

    build

--- fileIO.cpp
+++ fileIO.cpp
@@ -1,2 +1,50 @@
 #include "fileIO.h"
 
+fileIO::fileIO(const wchar_t* path, bool writeflag = false,int sheetnum = 0,const wchar_t* savepath=L"0")
+{
+	wstring paths(path);
+	const wchar_t* txtpath = L"./data/股票交易日志";
+	if(paths[13] == *txtpath)
+	{
+		isTxt = true;
+		txtfile.open(path);
+	}
+	else
+	{
+		book = xlCreateXMLBook();
+		book->setKey(L"TommoT", L"windows-2421220b07c2e10a6eb96768a2p7r6gc");
+		if (book->load(path))
+		{
+			sheetread = book->getSheet(sheetnum);
+			if (savepath == L"0")path_to_save = path;
+			else path_to_save = savepath;
+		}
+	}
+}
+
+fileIO::~fileIO()
+{
+	if (isTxt)
+	{
+		txtfile.close();
+	}
+	else
+	{
+		book->save(path_to_save);
+		book->release();
+	}
+}
+
+//此函数支持中文读取和显示，需加入否则中文内容乱码
+char* fileIO::w2c(char* pcstr, const wchar_t* pwstr, size_t len)
+{
+	//获取转换后的长度
+	int nlength = wcslen(pwstr);
+	//映射一个unicode字符串到一个多字节字符串
+	int nbytes = WideCharToMultiByte(0, 0, pwstr, nlength, NULL, 0, NULL, NULL); 
+	if (nbytes > len)nbytes = len;
+	// 通过以上得到的结果，转换unicode 字符为ascii 字符
+	WideCharToMultiByte(0, 0, pwstr, nlength, pcstr, nbytes, NULL, NULL);
+
+	return pcstr;
+}
--- fileIO.h
+++ fileIO.h
@@ -1,13 +1,31 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+#include "libxl.h"
+
+using namespace std;
+using namespace libxl;
+
 class fileIO
 {
 public:
-
-
-
+	fileIO(const wchar_t*, bool, int, const wchar_t*);
+	~fileIO();
+	char* w2c(char*, const wchar_t*, size_t);//此函数支持中文读取和显示，需加入否则中文内容乱码 
 
 private:
+	const wchar_t* path_to_save;
+	bool isTxt = false;
 
+	ifstream txtfile;
+	Book* book;
+	Sheet* sheetread;
+	CellType celltype;
+	Format* format;
+	string strcont;
+	double numcont;
 };
 
